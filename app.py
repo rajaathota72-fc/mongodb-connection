965553c48fcf01852758c8b2305355d558b1b201
rajaauditone <109960357+rajaauditone@users.noreply.github.com>
2023-07-24 18:03:05 +0200

    added mongo db default db and collection

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 from streamlit_mongodb.mongodb_connection import MongoDBConnection
 def main():
     # Read secrets from secrets.toml
-    st.secrets.load_manifest("secrets.toml")
+    #st.secrets.load_manifest("secrets.toml")
     mongo_secrets = st.secrets["mongodb"]
 
     # Establish a connection with MongoDB
@@ -20,23 +20,23 @@ def main():
         if title and content:
             if note_id:
                 update_data = {"title": title, "content": content}
-                conn.update_document("notes", note_id, update_data)
+                conn.update_document("Notebook","Notes", note_id, update_data)
                 st.success("Note updated successfully!")
             else:
                 document = {"title": title, "content": content}
-                conn.insert_document("notes", document)
+                conn.insert_document("Notebook","Notes", document)
                 st.success("Note added successfully!")
 
     # Show all notes
     st.subheader("All Notes:")
-    notes_df = conn.get_all_documents("notes")
+    notes_df = conn.get_all_documents("Notebook","Notes")
     st.dataframe(notes_df)
 
     # Delete a note
     if st.button("Delete Note"):
         note_id_to_delete = st.text_input("Enter Note ID to delete")
         if note_id_to_delete:
-            conn.delete_document("notes", note_id_to_delete)
+            conn.delete_document("Notebook","Notes", note_id_to_delete)
             st.success("Note deleted successfully!")
 
 if __name__ == "__main__":
--- streamlit_mongodb/mongodb_connection.py
+++ streamlit_mongodb/mongodb_connection.py
@@ -17,26 +17,26 @@ class MongoDBConnection(ExperimentalBaseConnection[MongoClient]):
     def connection(self) -> MongoClient:
         return self._instance
 
-    def insert_document(self, collection_name: str, document: dict):
+    def insert_document(self, db_name: str,collection_name: str, document: dict):
         connection = self.connection()
-        collection = connection.get_database()[collection_name]
+        collection = connection[db_name][collection_name]
         collection.insert_one(document)
 
-    def update_document(self, collection_name: str, document_id: str, update_data: dict):
+    def update_document(self, db_name: str, collection_name: str, document_id: str, update_data: dict):
         connection = self.connection()
-        collection = connection.get_database()[collection_name]
+        collection = connection[db_name][collection_name]
         filter_query = {"_id": document_id}
         update_query = {"$set": update_data}
         collection.update_one(filter_query, update_query)
 
-    def delete_document(self, collection_name: str, document_id: str):
+    def delete_document(self, db_name: str, collection_name: str, document_id: str):
         connection = self.connection()
-        collection = connection.get_database()[collection_name]
+        collection = connection[db_name][collection_name]
         collection.delete_one({"_id": document_id})
 
-    def get_all_documents(self, collection_name: str) -> pd.DataFrame:
+    def get_all_documents(self, db_name: str, collection_name: str) -> pd.DataFrame:
         connection = self.connection()
-        collection = connection.get_database()[collection_name]
+        collection = connection[db_name][collection_name]
         cursor = collection.find({})
         df = pd.DataFrame(list(cursor))
         return df
